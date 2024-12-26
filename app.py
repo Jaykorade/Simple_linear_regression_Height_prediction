7904fe4ab94a787c5a0f07afe3b59b98fed2ceae
Jaykorade <jaykorade@gmail.com>
2024-12-27 00:56:19 +0530

    push

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
-
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 # Load the pre-trained model
 with open('regressor.pkl', 'rb') as file:
     model = pickle.load(file)
@@ -10,8 +11,8 @@ st.title('Height Prediction App')
 
 # Input weight
 weight = st.number_input('Enter weight (kg)', min_value=0.0, step=0.1)
-
+#weight = scaler.fit_transform(weight)
 # Predict height
 if st.button('Predict Height'):
-    height = model.predict([[int(weight)]])
-    st.write(f'Predicted Height: {height[0]:.2f} cm')
+    height = model.predict([[weight]])
+    st.write(f'Predicted Height: {height}')
